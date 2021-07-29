964c7582aca520a9c11994aca9358a4af787ac3c
谭金杰 <tanjj02@mingyuan.com>
2021-07-29 10:45:34 +0800

    feat: add webpack5

--- webpack5/src/index.tsx
+++ webpack5/src/index.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDOM from "react-dom";
-import App from "./App";
+import ReactDOM from 'react-dom';
+import App from './App';
 import './index.less';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
